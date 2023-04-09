a43b534149b02f0224f515bdcb1480d4d18bdb17
sedat.akkus <sedat@base86.net>
2023-04-09 17:07:04 +0300

    inter milan

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@
         {
             Console.WriteLine("Mehmet");//V1
             Console.WriteLine("Veli");//V1
+            Console.WriteLine("1");//V1
+            Console.WriteLine("2");//V1
+            Console.WriteLine("3");//V1
+            Console.WriteLine("4");//V1
+            Console.WriteLine("5");//V1
         }
     }
 }
